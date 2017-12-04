bc18f5765be6b1579544ce464e608d0a48350522
Hetu <hetu@codebrahma.com>
2017-12-04 13:48:08 +0530

    Basic layout

---
+++ src/Containers/QuestionEditor/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const QuestionEditor = () => {
+  return (
+    <div className="QuestionEditor">
+      <p>QuestionEditor</p>    
+    </div>
+  );
+};
+
+export default QuestionEditor;
---
+++ src/Containers/QuestionList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const QuestionList = () => {
+  return (
+    <div className="QuestionList">
+      <p>QuestionList</p>
+    </div>
+  );
+};
+
+export default QuestionList;
