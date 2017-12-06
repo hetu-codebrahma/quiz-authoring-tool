38f5465f884aaaa1709ddf789515f594a1292209
Hetu <hetu@codebrahma.com>
2017-12-07 00:07:21 +0530

    Persist store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
-import reducers from './Store/';
+import store from './Store/';
 import QuestionList from './Containers/QuestionList';
 import QuestionEditor from './Containers/QuestionEditor';
 import './App.css';
 
-const store = createStore(reducers, applyMiddleware(logger));
 
 class App extends Component {
   render() {
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,6 +1,21 @@
-import { combineReducers } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import logger from 'redux-logger';
 import questions from './reducer';
 
-export default combineReducers({
+const reducers = combineReducers({
   questions,
 });
+
+const store = createStore(
+  reducers,
+  undefined,
+  compose(
+    applyMiddleware(logger),
+    autoRehydrate(),
+  ),
+);
+
+persistStore(store)
+
+export default store;
