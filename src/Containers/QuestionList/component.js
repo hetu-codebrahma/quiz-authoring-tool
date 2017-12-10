e7766f1e1f716e249e20cc036d5807bdf159674d
Hetu <hetu@codebrahma.com>
2017-12-10 19:03:17 +0530

    Mobile friendly

--- src/Containers/QuestionList/component.js
+++ src/Containers/QuestionList/component.js
@@ -10,6 +10,7 @@ class QuestionList extends Component {
     super(props);
     this.state = {
       deleteMode: false,
+      showList: false,
     }
   }
 
@@ -46,58 +47,93 @@ class QuestionList extends Component {
     })
   }
 
+  onShowListButtonClicked() {
+    this.setState({
+      showList: true,
+    })
+  }
+
+  onHideListButtonClicked() {
+    this.setState({
+      showList: false,
+    })
+  }
+
   render(){
     const { list, selectedQuestion } = this.props.questions;
+    const { showList } = this.state;
+    let QuestionListClasses = 'QuestionList';
+    if(showList){
+      QuestionListClasses = QuestionListClasses + ' QuestionListMobileDisplay'
+    }
+     
     return (
-      <div className="QuestionList">
-        <h3 className="heading">List of questions</h3>
-        <div>
-          {
-            list.map((question, i) => {
-              return (
-                <div key={question.id} className="QuestionListItemContainer">
-                  {this.state.deleteMode ? (
-                    <Button
-                      onClick={() => this.deleteQuestion(question.id)}
-                      label="X"
-                      labelColor="white"
-                      backgroundColor="#e74c3c"
-                    />
-                  ) : <span className="QuestionIndex">{i + 1}.</span>}
-                  <QuestionListItem
-                    question={question}
-                    selected={selectedQuestion === question.id}
-                    onQuestionClicked={() => this.selectQuestion(question.id)}
-                  />
-                </div>
-              );
-            })
-          }
+      <div className="QuestionListContainer">
+        <div className="QuestionListButton">
+          {!showList && (
+            <Button 
+              label={<i className="material-icons">menu</i>}
+              onClick={() => this.onShowListButtonClicked()}
+            />  
+          )}
         </div>
-        {this.state.deleteMode ? (
-          <div className="ButtonsContainer">
-            <Button
-              onClick={() => this.deactivateDeleteMode()}
-              label="Done"
-              backgroundColor="white"              
-            />
-          </div>
-        ) : (
-          <div className="ButtonsContainer">
-            <Button
-              onClick={() => this.addNewQuestion()}
-              label="Add"
-              backgroundColor="#CDDC39"
-              labelColor="#333"
-            />
-            <Button
-              onClick={() => this.activateDeleteMode()}
-              label="Delete"
-              backgroundColor="crimson"
+        <div className={QuestionListClasses}>
+          <div className="QuestionListButton">    
+            <Button 
+              label={<i className="material-icons">close</i>}
+              onClick={() => this.onHideListButtonClicked()}
               labelColor="white"
-            />
+            />  
           </div>
-        )}
+          <h3 className="heading">List of questions</h3>
+          <div>
+            {
+              list.map((question, i) => {
+                return (
+                  <div key={question.id} className="QuestionListItemContainer">
+                    {this.state.deleteMode ? (
+                      <Button
+                        onClick={() => this.deleteQuestion(question.id)}
+                        label="X"
+                        labelColor="white"
+                        backgroundColor="#e74c3c"
+                      />
+                    ) : <span className="QuestionIndex">{i + 1}.</span>}
+                    <QuestionListItem
+                      question={question}
+                      selected={selectedQuestion === question.id}
+                      onQuestionClicked={() => this.selectQuestion(question.id)}
+                    />
+                  </div>
+                );
+              })
+            }
+          </div>
+          {this.state.deleteMode ? (
+            <div className="ButtonsContainer">
+              <Button
+                onClick={() => this.deactivateDeleteMode()}
+                label="Done"
+                backgroundColor="white"              
+              />
+            </div>
+          ) : (
+            <div className="ButtonsContainer">
+              <Button
+                onClick={() => this.addNewQuestion()}
+                label="Add"
+                backgroundColor="#CDDC39"
+                labelColor="#333"
+              />
+              <Button
+                onClick={() => this.activateDeleteMode()}
+                label="Delete"
+                backgroundColor="crimson"
+                labelColor="white"
+              />
+            </div>
+          )}
+        </div>
       </div>
     );
   }
